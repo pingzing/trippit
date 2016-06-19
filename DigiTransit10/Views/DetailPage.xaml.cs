d636679e401d17324954211682bb5f91ecdd306d
pingzing <mcalistern@gmail.com>
2016-06-19 17:34:10 +0300

    Add project files.

---
+++ DigiTransit10/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using DigiTransit10.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace DigiTransit10.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
