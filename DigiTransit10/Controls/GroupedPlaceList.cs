0182ffdf7810e70c19abe9399fff09506a70d4d5
pingzing <neil.mcalister@futurice.com>
2016-07-05 17:34:20 +0300

    Add files I missed in last commit >.>

---
+++ DigiTransit10/Controls/GroupedPlaceList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DigiTransit10.Models;
+
+namespace DigiTransit10.Controls
+{
+    public class GroupedPlaceList : ObservableCollection<Place>
+    {
+        public string Key { get; }
+        public ModelEnums.PlaceType GroupType { get; }
+
+        public GroupedPlaceList(ModelEnums.PlaceType type, string header)
+        {
+            GroupType = type;
+            Key = header;
+        }
+    }
+}
