ae7e2a296e2076fbbfe4c4cd8f6970ccf99f8658
pingzing <mcalistern@gmail.com>
2016-07-09 14:34:10 +0300

    Checkpoint commit--TripPlanStrip now using 2-pass render to get the transitIcons aligned properly

--- DigiTransit10/Controls/TripPlanStrip.xaml.cs
+++ DigiTransit10/Controls/TripPlanStrip.xaml.cs
@@ -18,6 +18,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
 
 // The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
 
@@ -84,6 +85,7 @@ namespace DigiTransit10.Controls
         private void CreateNewPlanStrip(ApiItinerary thisItinerary)
         {
             TripLegs.Clear();
+            loadedGrids.Clear();
             for(int i = 0; i < thisItinerary.Legs.Count; i++)
             {
                 bool isEnd = i == thisItinerary.Legs.Count - 1;
@@ -106,7 +108,77 @@ namespace DigiTransit10.Controls
         private void RaisePropertyChanged([CallerMemberName]string property = "")
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
-        }        
+        }
+
+        List<Grid> loadedGrids = new List<Grid>();
+        private void LegContentRoot_Loaded(object sender, RoutedEventArgs e)
+        {
+            Grid grid = sender as Grid;
+            if(grid == null)
+            {
+                return;
+            }
+            loadedGrids.Add(grid);
+            if(loadedGrids.Count == _tripLegs.Count)
+            {
+                RepositionIcons(loadedGrids);
+            }            
+        }
+
+        private void RepositionIcons(List<Grid> loadedGrids)
+        {
+            foreach (var grid in loadedGrids)
+            {
+                var firstPoint = (grid.Children.FirstOrDefault() as TripPlanPoint)
+                    ?.TripPlanPointRootLayout?.Children
+                    ?.Skip(1)?.Take(1)
+                    ?.FirstOrDefault() as Ellipse;
+                if (firstPoint == null)
+                {
+                    return;
+                }
+
+                var potentialEndpoint = (grid.Children.LastOrDefault() as TripPlanPoint);
+                Ellipse thirdPoint = null;
+                if (potentialEndpoint.Visibility != Visibility.Collapsed)
+                {
+                    thirdPoint = potentialEndpoint?.TripPlanPointRootLayout?
+                        .Children
+                        ?.Skip(1)?.Take(1)
+                        ?.FirstOrDefault() as Ellipse;
+                }                
+                
+                if (thirdPoint == null)
+                {
+                    var currentVm = grid.Tag as TripLegViewModel;
+                    var targetVm = _tripLegs[_tripLegs.IndexOf(currentVm) + 1];
+                    var newGrid = loadedGrids.Where(x => x.Tag == targetVm).FirstOrDefault();
+                    if(newGrid == null)
+                    {
+                        return;
+                    }
+                    thirdPoint = (newGrid.Children.FirstOrDefault() as TripPlanPoint)
+                        ?.TripPlanPointRootLayout?.Children
+                        ?.Skip(1)?.Take(1)
+                        ?.FirstOrDefault() as Ellipse;
+                }
+                
+                if (thirdPoint == null)
+                {
+                    return;
+                }
+
+                var icon = (grid.Children.Skip(1).Take(1).First() as TripPlanTransitMethod);
+
+                double firstPointCenterX = firstPoint.TransformToVisual(Window.Current.Content).TransformPoint(new Point(0, 0)).X + 10;
+                double iconCenterX = icon.TransformToVisual(Window.Current.Content).TransformPoint(new Point(0, 0)).X + (icon.ActualWidth / 2);
+                double thirdPointCenterX = thirdPoint.TransformToVisual(Window.Current.Content).TransformPoint(new Point(0, 0)).X + 10;                
+                double circlesMidpoint = (firstPointCenterX + thirdPointCenterX) / 2;
+
+                double difference = circlesMidpoint - iconCenterX;
+                icon.RenderTransform = new TranslateTransform { X = difference };                                          
+            }
+        }
     }
 
     public class TripLegViewModel
