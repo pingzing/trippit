3671c6c4db562d47bec63d5f83afda8a10e0e732
pingzing <mcalistern@gmail.com>
2016-08-01 11:29:43 +0300

    Refactor fade-down animation into reusable behavior

---
+++ DigiTransit10/Behaviors/AnimateVisibilityBehavior.cs
@@ -0,0 +1,47 @@
+﻿using DigiTransit10.Storyboards;
+using Microsoft.Xaml.Interactivity;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media.Animation;
+
+namespace DigiTransit10.Behaviors
+{
+    public class AnimateVisibilityBehavior : DependencyObject, IBehavior
+    {
+        long _callbackToken;
+        Storyboard _animationStoryboard = null;
+
+        public DependencyObject AssociatedObject { get; private set; }
+
+        public void Attach(DependencyObject associatedObject)
+        {
+            AssociatedObject = associatedObject;
+            _callbackToken = AssociatedObject.RegisterPropertyChangedCallback(UIElement.VisibilityProperty, OnVisibilityChanged);
+        }
+
+        public void Detach()
+        {
+            AssociatedObject.UnregisterPropertyChangedCallback(UIElement.VisibilityProperty, _callbackToken);
+        }
+
+        private void OnVisibilityChanged(DependencyObject sender, DependencyProperty dp)
+        {
+            UIElement _this = sender as UIElement;
+            if(_this == null)
+            {
+                return;
+            }
+
+            if ((Visibility)_this.GetValue(dp) == Visibility.Collapsed)
+            {
+                return;
+            }
+
+            if (_animationStoryboard == null)
+            {
+                _animationStoryboard = FadeInDownwardFactory.GetAnimation(_this);
+            }            
+
+            _animationStoryboard.Begin();            
+        }
+    }
+}
---
+++ DigiTransit10/Storyboards/FadeInDownwardFactory.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media.Animation;
+
+namespace DigiTransit10.Storyboards
+{
+    /// <summary>
+    /// A helper class for creating FadeInDownward storyboard animations.
+    /// </summary>
+    public static class FadeInDownwardFactory
+    {                 
+        /// <summary>
+        /// Returns a storyboard object that applies a gentle fade-and-downward-swoop to the input element.
+        /// </summary>
+        /// <param name="target">The object to apply the storyboard animation to.</param>
+        /// <returns>A <see cref="Storyboard"/> that defines the animation.</returns>
+        public static Storyboard GetAnimation(DependencyObject target)
+        {
+            var storyboard = new Storyboard();
+
+            RepositionThemeAnimation repositionAnimation = new RepositionThemeAnimation
+            {
+                FromVerticalOffset = -50
+            };
+            Storyboard.SetTarget(repositionAnimation, target);
+
+            var opacityAnimation = new DoubleAnimationUsingKeyFrames();
+            var opacityStartFrame = new EasingDoubleKeyFrame();
+            opacityStartFrame.KeyTime = KeyTime.FromTimeSpan(TimeSpan.FromMilliseconds(0));
+            opacityStartFrame.Value = 0;
+
+            var opacityEndFrame = new EasingDoubleKeyFrame();
+            opacityEndFrame.KeyTime = KeyTime.FromTimeSpan(TimeSpan.FromMilliseconds(250));
+            opacityEndFrame.Value = 1;
+
+            opacityAnimation.KeyFrames.Add(opacityStartFrame);
+            opacityAnimation.KeyFrames.Add(opacityEndFrame);
+
+            Storyboard.SetTargetProperty(opacityAnimation, "Opacity");
+            Storyboard.SetTarget(opacityAnimation, target);            
+
+            storyboard.Children.Add(repositionAnimation);
+            storyboard.Children.Add(opacityAnimation);
+            return storyboard;
+        }
+    }
+}
