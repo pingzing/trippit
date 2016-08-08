a6a9b7eee2273aff232b727d2b4422c99e928da6
pingzing <mcalistern@gmail.com>
2016-08-09 00:07:11 +0300

    Add permanently-visible scrollbar to search box

---
+++ DigiTransit10/ExtensionMethods/DictionaryExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigiTransit10.ExtensionMethods
+{
+    public static class DictionaryExtensions
+    {
+        public static void AddOrUpdate<TKey, TValue>(this IDictionary<TKey, TValue> thisDict, TKey key, TValue value)
+        {
+            if(thisDict.ContainsKey(key))
+            {
+                thisDict[key] = value;
+            }
+            else
+            {
+                thisDict.Add(key, value);
+            }
+        }
+    }
+}
