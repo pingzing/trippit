73d234bf67926304d2b2f1a3bd0d4ed8f9566576
Neil McAlister <neil.mcalister@futurice.com>
2016-11-19 19:22:45 +0200

    Add some plumbing for viewing a stop's details

---
+++ DigiTransit10/ViewModels/ControlViewModels/StopSearchContentViewModel.cs
@@ -0,0 +1,46 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Template10.Common;
+using Template10.Mvvm;
+
+namespace DigiTransit10.ViewModels.ControlViewModels
+{
+    public class StopSearchContentViewModel : BindableBase
+    {
+        public enum StopSearchState { Overview, Details };
+
+        private StopSearchState _currentState;
+        public StopSearchState CurrentState
+        {
+            get { return _currentState; }
+            set { Set(ref _currentState, value); }
+        }
+
+        public RelayCommand LoadedCommand => new RelayCommand(Loaded);
+        public RelayCommand UnloadedCommand => new RelayCommand(Unloaded);
+
+        private void Loaded()
+        {
+            BootStrapper.BackRequested += BootStrapper_BackRequested;
+        }
+
+        private void Unloaded()
+        {
+            BootStrapper.BackRequested -= BootStrapper_BackRequested;
+        }
+
+        private void BootStrapper_BackRequested(object sender, HandledEventArgs e)
+        {
+            if (CurrentState == StopSearchState.Details)
+            {
+                _currentState = StopSearchState.Overview;
+                e.Handled = true;
+                //go to Overview somehow                
+            }
+        }
+    }
+}
---
+++ DigiTransit10/ViewModels/ControlViewModels/StopSearchElementViewModel.cs
@@ -0,0 +1,37 @@
+﻿using DigiTransit10.Models;
+using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Template10.Common;
+using Template10.Mvvm;
+using Template10.Services.NavigationService;
+
+namespace DigiTransit10.ViewModels.ControlViewModels
+{
+    public class StopSearchElementViewModel : BindableBase
+    {        
+        private readonly INavigationService _navService;
+
+        private TransitStop _backingStop;
+        public TransitStop BackingStop
+        {
+            get { return _backingStop; }
+            set { Set(ref _backingStop, value); }
+        }
+
+        public RelayCommand ViewDetailsCommand => new RelayCommand(ViewDetails);        
+
+        public StopSearchElementViewModel(TransitStop backingStop)
+        {
+            BackingStop = backingStop;
+        }        
+
+        private void ViewDetails()
+        {
+            //send a message to parent asking it to move into the detailed state.  
+        }       
+    }
+}
