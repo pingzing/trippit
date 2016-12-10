947b16628185ef50253aa67213e68ba5d5b4d6ff
pingzing <mcalistern@gmail.com>
2016-12-10 11:31:52 +0200

    Add control vm

---
+++ DigiTransit10/ViewModels/ControlViewModels/TransitStopViewModel.cs
@@ -0,0 +1,29 @@
+﻿using DigiTransit10.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Template10.Mvvm;
+
+namespace DigiTransit10.ViewModels.ControlViewModels
+{
+    public class TransitStopViewModel : BindableBase
+    {
+        public TransitStop BackingStop { get; set; }
+        public string NameAndCode
+        {
+            get
+            {
+                if (!String.IsNullOrWhiteSpace(BackingStop.Code))
+                {
+                    return $"{BackingStop.Name}, {BackingStop.Code}";
+                }
+                else
+                {
+                    return BackingStop.Name;
+                }
+            }
+        }
+    }
+}
